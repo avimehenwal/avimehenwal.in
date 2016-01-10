ae3495c8ccadd8cf5e97be4f13a87b08edc5c4fd
avimehenwal <avi.mehanwal@gmail.com>
2016-01-10 21:26:33 +0530

    models and related configs updated

---
+++ blog.avi.com/blogavicom/models/blog_record.py
@@ -0,0 +1,9 @@
+import datetime
+from blogavicom.models.meta import Base
+from sqlalchemy import (
+    Column,
+    Integer,
+    Unicode,     #<- will provide Unicode field
+    UnicodeText, #<- will provide Unicode text field
+    DateTime,    #<- time abstraction field
+    )
