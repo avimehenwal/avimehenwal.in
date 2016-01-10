3e7eaf78213e4980f254df89064222101e24e6c9
avimehenwal <avi.mehanwal@gmail.com>
2016-01-10 23:05:55 +0530

    :Adding and editing blog entries

--- blog.avi.com/blogavicom/views/blog.py
+++ blog.avi.com/blogavicom/views/blog.py
@@ -3,6 +3,7 @@ from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 from ..models.meta import DBSession
 from ..models.blog_record import BlogRecord
 from ..models.services.blog_record import BlogRecordService
+from ..forms import BlogCreateForm, BlogUpdateForm
 
 
 @view_config(route_name='blog',
@@ -17,9 +18,24 @@ def blog_view(request):
 @view_config(route_name='blog_action', match_param='action=create',
              renderer='pyramid_blogr:templates/edit_blog.jinja2')
 def blog_create(request):
-    return {}
+    entry = BlogRecord()
+    form = BlogCreateForm(request.POST)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        DBSession.add(entry)
+        return HTTPFound(location=request.route_url('home'))
+    return {'form': form, 'action': request.matchdict.get('action')}
 
 @view_config(route_name='blog_action', match_param='action=edit',
              renderer='pyramid_blogr:templates/edit_blog.jinja2')
 def blog_update(request):
-    return {}
+    blog_id = int(request.params.get('id', -1))
+    entry = BlogRecordService.by_id(blog_id)
+    if not entry:
+        return HTTPNotFound()
+    form = BlogUpdateForm(request.POST, entry)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        return HTTPFound(location=request.route_url('blog', id=entry.id,
+                                                    slug=entry.slug))
+    return {'form': form, 'action': request.matchdict.get('action')}
