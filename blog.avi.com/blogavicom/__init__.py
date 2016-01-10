7b99074bf957dd519851d35ca9cda6820c61af54
avimehenwal <avi.mehanwal@gmail.com>
2016-01-10 22:09:33 +0530

    added routes

--- blog.avi.com/blogavicom/__init__.py
+++ blog.avi.com/blogavicom/__init__.py
@@ -16,6 +16,11 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
+    config.add_route('blog', '/blog/{id:\d+}/{slug}')
+    config.add_route('blog_action', '/blog/{action}')
+    config.add_route('auth', '/sign/{action}')
+
     config.scan()
     return config.make_wsgi_app()
---
+++ blog.avi.com/blogavicom/views/default.py
@@ -0,0 +1,13 @@
+from pyramid.view import view_config
+
+
+@view_config(route_name='home',
+    renderer='blog.avi.com:templates/index.jinja2')
+def index_page(request):
+    return {}
+
+@view_config(route_name='auth', match_param='action=in', renderer='string',
+             request_method='POST')
+@view_config(route_name='auth', match_param='action=out', renderer='string')
+def sign_in_out(request):
+    return {}
