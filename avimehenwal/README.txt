3ffbcb760834f57d7839d9591852c27c150240b3
avimehenwal <avi.mehanwal@gmail.com>
2016-01-30 23:00:18 +0530

    : model created from scaffold

---
+++ avimehenwal/README.txt
@@ -0,0 +1,14 @@
+avimehenwal README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/initialize_avimehenwal_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ avimehenwal/avimehenwal/models.py
@@ -0,0 +1,42 @@
+import datetime #<- will be used to set default dates on models
+
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Text,
+    Unicode,     #<- will provide Unicode field
+    UnicodeText, #<- will provide Unicode text field
+    DateTime,    #<- time abstraction field
+    )
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
+
+class MyModel(Base):
+    __tablename__ = 'models'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    value = Column(Integer)
+
+Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+
+class BlogRecord(Base):
+    __tablename__ = 'entries'
+    id       = Column(Integer, primary_key=True)
+    title    = Column(Unicode(255), unique=True, nullable=False)
+    body     = Column(UnicodeText, default=u'')
+    author   = Column(UnicodeText, default=u'avimehenwal')
+    created  = Column(DateTime, default=datetime.datetime.utcnow)
+    modified = Column(DateTime, default=datetime.datetime.utcnow)
