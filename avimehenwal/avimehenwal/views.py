f8b89ec95c03231728fcc289588a677f6d3b916b
avimehenwal <avi.mehanwal@gmail.com>
2016-01-31 00:20:52 +0530

    : +model services

--- avimehenwal/avimehenwal/views.py
+++ avimehenwal/avimehenwal/views.py
@@ -6,31 +6,26 @@ from sqlalchemy.exc import DBAPIError
 from .models import (
     DBSession,
     MyModel,
+    BlogRecordService,
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home',
+    renderer='avimehenwal:templates/index.jinja2')
 def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'avimehenwal'}
+    page = int(request.params.get('page', 1))
+    paginator = BlogRecordService.get_paginator(request, page)
+    return {'paginator': paginator}
+
+@view_config(route_name='blog_action',
+    match_param='action=create',
+    renderer='avimehenwal:templates/edit_blog.jinja')
+def blog_create(request):
+    return Response('Create Blog')
 
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_avimehenwal_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
-
+@view_config(route_name='blog_action',
+    match_param='action=edit',
+    renderer='avimehenwal:templates/edit_blog.jinja')
+def blog_create(request):
+    return Request('Edit Blog') 
